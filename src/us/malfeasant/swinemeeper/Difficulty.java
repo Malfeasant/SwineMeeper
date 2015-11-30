869058ddad23b357d801efbd58839fda2b8e88f8
Mischa Sumrall <madchao@yahoo.com>
2015-11-29 18:42:11 -0700

    Fleshing it out

--- src/us/malfeasant/swinemeeper/Difficulty.java
+++ src/us/malfeasant/swinemeeper/Difficulty.java
@@ -1,7 +1,7 @@
 package us.malfeasant.swinemeeper;
 
 public enum Difficulty {
-	EASY(8, 8, 10), MEDIUM(16, 16, 40), HARD(40, 20, 99), CUSTOM(0, 0, 0) {
+	EASY(8, 8, 10), MEDIUM(16, 16, 40), HARD(30, 16, 99), CUSTOM(0, 0, 0) {
 		@Override
 		public int getWidth() {
 			return Persist.getCustomWidth();
--- src/us/malfeasant/swinemeeper/Launcher.java
+++ src/us/malfeasant/swinemeeper/Launcher.java
@@ -1,13 +1,45 @@
 package us.malfeasant.swinemeeper;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
-
+	public static void main(String[] args) {
+		launch(args);
+	}
+	private final GridPane gameGrid = new GridPane();
+	private final Label timeLabel = new Label("000");
+	private final Label mineLabel = new Label("000");
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Difficulty diff = Persist.loadDifficulty();
+		Button go = new Button("Go");
+		go.setOnAction(e -> newGame());
+		BorderPane pane = new BorderPane();
+		pane.setCenter(go);
+		pane.setLeft(timeLabel);
+		pane.setRight(mineLabel);
+		pane.setBottom(gameGrid);
+		Scene scene = new Scene(pane);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("SwineMeeper");
+		primaryStage.show();
+		go.fire();
 	}
-
+	
+	private void newGame() {
+		gameGrid.getChildren().clear();
+		Difficulty diff = Persist.loadDifficulty();
+		for (int y = 0; y < diff.getHeight(); ++y) {
+			for (int x = 0; x < diff.getWidth(); ++x) {
+				gameGrid.add(new Button(" "), x, y);
+			}
+		}
+		
+	}
 }
